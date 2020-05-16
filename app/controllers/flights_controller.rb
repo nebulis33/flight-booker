ef581bbd8f4ee27edfb934cebade783bbb6047f0
nebulis33 <>
2020-05-16 11:51:41 -0700

    added functionality for search

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,9 +1,17 @@
 class FlightsController < ApplicationController
+    before_action :sanitize_params
+
     def index
-        @airport_options = Airport.all.map{ |a| [a.airport_code, a.id] } #loading everytime, scope this or something so it loads once
+        @airport_options = Airport.all.map{ |a| [a.airport_code, a.id] } #loading everytime, scope this or something so it loads once, maybe its the render
         @days = Flight.all.map{ |f| f.start_datetime }
-        @flights = Flight.where("from_airport_id = ? AND to_airport_id = ?", params[:from_airport], params[:to_airport]).map{ |f| f.id }
+        @flights = Flight.where("from_airport_id = ? AND to_airport_id = ?", @departure, @arrival).map{ |f| f.id }
         render "flights/index"
     end
 
+    private
+
+        def sanitize_params
+            @departure = params[:from_airport].to_i
+            @arrival = params[:to_airport].to_i
+        end
 end
