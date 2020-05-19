41d313351a0e34fc6a05679d6882c27719ffb680
nebulis33 <>
2020-05-18 17:11:50 -0700

    finally fixed the form error, needed to add local true to form so page could render the processed data after submit

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -5,8 +5,9 @@ class FlightsController < ApplicationController
         @airport_options = Airport.all.map{ |a| [a.airport_code, a.id] } #loading everytime, scope this or something so it loads once, maybe its the render
         @days = Flight.all.map{ |f| f.start_datetime }
         @flights = Flight.where("from_airport_id = ? AND to_airport_id = ?", @departure, @arrival).map{ |f| f.id }
-        render "flights/index"
+        #render "flights/index"
     end
+
 
     private
 
